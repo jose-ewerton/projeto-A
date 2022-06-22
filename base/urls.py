64c9a2e5a0f3eec823f05e522f2a263c3fbe8b0c
Jose Ewerton da Cruz de Souza <jose.ewerton@escolar.ifrn.edu.br>
2022-06-22 17:06:32 -0300

    Criando views e urls

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path, include
-from .views import HomeView
+from django.urls import path
+from .views import HomeView #view_home, view_about, view_contact
+
 
 urlpatterns = [
-    path('', HomeView.as_view(), name = "home"),
+    path('', HomeView.as_view(), name = 'home'),
+    #path('home/', view_home ),
+    #path('about/', view_about),
+    #path('contact/', view_contact),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+#from django.http import HttpResponse
+
 from django.views.generic import TemplateView
 
 # Create your views here.
+'''
+def view_home(request):
+    return HttpResponse("Página Home")
+
+def view_about(request):
+    return HttpResponse("Página About")
+
+def view_contact(request):
+    return HttpResponse("Página Contacts")
+'''
+
+'''
+def view_home(request):
+    return render(request, "home.html")
+
+'''
 class HomeView(TemplateView):
-    template_name = "home.html"
+    template_name = "base/home.html"
+
+
+
